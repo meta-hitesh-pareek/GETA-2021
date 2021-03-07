594808acb887e6cbfadaadf866eb855b4fef71da
meta-hitesh-pareek <hitesh.pareek@metacube.com>
2021-03-07 18:43:39 +0530

    change in SCF-assignment-6

--- SCF/SCF-assignment-6/IntSet.java
+++ SCF/SCF-assignment-6/IntSet.java
@@ -1,105 +1,135 @@
 package assignment6;
 
-public final class IntSet {
-	
-	final private int arr[]=new int[1000];
-	
-IntSet( int arr[])
-{
-    for(int i=0;i<arr.length;i++)
-    {
-    	this.arr[arr[i]-1]=1;
-    	
-    }
-	
-}	
-final public int[] printSet()
-{
-	final int thisArray[]=this.arr;
-    int newArray[]=new int[this.size()];
-    int k=0;
-    for(int j=0;j<1000;j++)
-    {
-    	if(thisArray[j]==1)
-    	{
-    		newArray[k]=j+1;
-    		k++;
-    	}
-    }
-    
-    return newArray;
-}
-final public boolean isMember(final int x)
-{
-	if(arr[x-1]==1)
-		return true;
-	else
+/**
+ * Perform operation between two sets
+ * 
+ * @since 07-03-2021
+ * @author Hitesh Pareek
+ *
+ */
+public class IntSet {
+	int set[];
+
+	/**
+	 * Initialize elements of given set
+	 * 
+	 * @param inputSet
+	 */
+	IntSet(int inputSet[]) {
+		this.set = inputSet;
+	}
+
+	/**
+	 * Prints the elements of the set
+	 */
+	public void printSet() {
+		for (int index = 0; index < set.length; index++) {
+			System.out.print(set[index] + " ");
+		}
+	}
+
+	/**
+	 * Checks if given value is a member of set
+	 * 
+	 * @param inputValue
+	 * @return true if it is a member, else false
+	 */
+	public boolean isMember(int inputValue) {
+		for (int index = 0; index < set.length; index++) {
+			if (set[index] == inputValue) {
+				return true;
+			}
+		}
 		return false;
-}
-final public int size()
-{
-	int size=0;
-	for(int i=0;i<arr.length;i++)
-	{
-		if(arr[i]==1)
-		{
-			size++;
-			
+	}
+
+	/**
+	 * Find the size of set
+	 * 
+	 * @return size of set
+	 */
+	public int size() {
+		return set.length;
+	}
+
+	/**
+	 * Check if a given set is a subset of current set or not
+	 * 
+	 * @param compareSet
+	 * @return true if given set is a subset, else false
+	 */
+	public boolean isSubset(IntSet compareSet) {
+		int inputSet[] = compareSet.set;
+
+		if (this.size() != compareSet.size()) {
+			return false;
 		}
-	}
-	return size;
-	
-}
-final public  boolean isSubset(IntSet obj)
-{
-	boolean flag=true;
-	final int thisArray[]=this.arr;
-	final int setArray[]=obj.arr;
-	
-	for(int i=0;i<1000;i++)
-	{
-		if(setArray[i]==1 && thisArray[i]!=1)
-		{
-			flag=false;
-			break;
+		for (int index = 0; index < this.size(); index++) {
+			boolean flag = false;
+
+			for (int compareIndex = 0; compareIndex < compareSet.size(); compareIndex++) {
+				if (set[index] == inputSet[compareIndex]) {
+					flag = true;
+				}
+			}
+
+			if (flag == false) {
+				return false;
+			}
 		}
+		return true;
 	}
-	return flag;
-	
-	
-}
-final public IntSet getComplement()
-{
-	final int thisArray[]=this.arr;
-    final int size=1000-(this.size());
-    final int newArray[]=new int[size];
-    int j=0;
-    for(int i=0;i<1000;i++)
-    {
-      if(thisArray[i]==0)
-      {
-    	  newArray[j]=i+1;
-    	  j++;
-      }
-    	
-    }
-    IntSet obj =new IntSet(newArray);
-    return obj;
-}
-	public static void main(String args[])
-	{
-		int arr[]={1,2,4,5};
-		int arr2[]={2,4,1,5};
-	IntSet obj1=new IntSet(arr);
-    IntSet obj2=new IntSet(arr2);
-    
-    
-//    System.out.println(obj1.size());
-//    System.out.println(obj1.isSubset(obj2));
-    IntSet s2=obj1.getComplement();
-    int newArray[]=s2.printSet();    
-    for(int x:newArray)
-    	System.out.print(x+" ");
+
+	/**
+	 * Finds the complement of the current set
+	 * 
+	 * @param userSet
+	 * @return complement
+	 */
+	public IntSet getComplement() {
+		int currentSet[]=this.set;
+		int resultSet[] = new int[1000 - currentSet.length];
+		int position = 0;
+
+		for (int index = 1; index <= 1000; index++) {
+			if (!this.isMember(index)) {
+				resultSet[position++] = index;
+			}
+		}
+		IntSet complimentSet = new IntSet(resultSet);
+		return complimentSet;
 	}
-	
+
+	/**
+	 * Create a union set of the two given set
+	 * 
+	 * @param firstInput
+	 * @param secondInput
+	 * @return union set of given two set
+	 */
+	public IntSet union(IntSet firstInput, IntSet secondInput) {
+		int firstSet[] = firstInput.set;
+		int secondSet[] = secondInput.set;
+
+		int resultsize = 0;
+		for (int index = 0; index < firstSet.length; index++) {
+			if (!secondInput.isMember(firstSet[index])) {
+				resultsize++;
+			}
+		}
+		resultsize += firstSet.length;
+		int resultSet[] = new int[resultsize];
+		int currentPosition = 0;
+		for (int index = 0; index < firstSet.length; index++) {
+				resultSet[currentPosition++] = firstSet[index];
+		}
+
+		for (int index = 0; index < secondSet.length; index++) {
+			if (!firstInput.isMember(secondSet[index])) {
+				resultSet[currentPosition++] = secondSet[index];
+			}
+		}
+		IntSet outputSet = new IntSet(resultSet);
+		return outputSet;
+	}
 }
